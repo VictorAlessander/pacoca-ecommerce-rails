4ffc5a0a5adc7a28a40622027aee9ca26f5ae3f1
Victor Alessander <alessander@protonmail.com>
2017-06-09 17:14:48 -0300

    Added rspec test for model 'Account' and some little changes

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,3 +1,5 @@
 class Account < ApplicationRecord
   belongs_to :person
+
+  validates_presence_of :username, :password
 end
---
+++ spec/models/account_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+require 'faker'
+
+
+RSpec.describe Account, type: :model do
+	let(:person) {Person.create(name: Faker::Name.name, birthday: Faker::Date.birthday, email: Faker::Internet.email)}
+  subject {described_class.new}
+
+  it "Valid with all attributes" do
+    subject.username = Faker::Name.first_name
+    subject.password = Faker::Lorem.word
+    subject.person_id = person.id
+    expect(subject).to be_valid
+  end
+
+  it "Not valid without person relationship" do
+  	expect(subject).to_not be_valid
+  end
+
+  it "Not valid without username" do
+  	subject.person_id = person.id
+  	expect(subject).to_not be_valid
+  end
+
+  it "Not valid without password" do
+  	subject.username = Faker::Name.first_name
+  	subject.person_id = person.id
+  	expect(subject).to_not be_valid
+  end
+end
