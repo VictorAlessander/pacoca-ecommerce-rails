3bf8b1e0efd3a652069d555ef0d38decebcb44d3
alessander <alessander@protonmail.com>
2017-06-16 10:39:05 -0400

    Implemented Factory Girl to tests, fixed old tests, added more tests and some little changes

--- spec/controllers/people_controller_spec.rb
+++ spec/controllers/people_controller_spec.rb
@@ -4,10 +4,15 @@ require 'faker'
 
 RSpec.describe PeopleController do
 
-	let(:person) {create :person}
+	before :each do
+		@person = create(:person,
+			name: 'Joaozinho',
+			birthday: "1990-04-28",
+			email: 'joao.zinho@umbrellacorporation.com')
+	end
 
 	describe 'GET #index' do
-		it 'renders the :index view' do
+		it 'Renders the :index view' do
 			get :index
 			expect(response).to render_template(:index)
 		end
@@ -15,62 +20,92 @@ RSpec.describe PeopleController do
 
 
 	describe 'GET #new' do
-		it 'renders the :new view' do
+		it 'Renders the :new view' do
 			get :new
 			expect(response).to render_template(:new)
 		end
 
-		it 'get 200 status code' do
+		it 'Get 200 status code' do
 			get :new
 			expect(response.status).to eq(200)
 		end
 	end
 
-	# INCOMPLETE
-
 	describe 'POST #create' do
-		context 'with valid attributes' do
-			it 'creates a new person' do
+		context 'With valid attributes' do
+			it 'Creates a new person' do
 				expect{
-					post :create, params: FactoryGirl.attributes_for(:person)
+					post :create, params: {person: FactoryGirl.attributes_for(:person)}
 					}.to change{Person.count}.by(1)
 			end
 		end
 	end
 
 	describe 'GET #edit' do
-		it 'renders the :edit view' do
-			get :edit, params: {id: person.id}
+		it 'Renders the :edit view' do
+			get :edit, params: {id: @person.id}
 			expect(response).to render_template(:edit)
 		end
 
-		it 'get 200 status code' do
-			get :edit, params: {id: person.id}
+		it 'Get 200 status code' do
+			get :edit, params: {id: @person.id}
 			expect(response.status).to eq(200)
 		end
 	end
 
 	describe 'GET #show' do
-		it 'renders the :show view' do
-			get :show, params: {id: person.id}
+		it 'Renders the :show view' do
+			get :show, params: {id: @person.id}
 			expect(response).to render_template(:show)
 		end
 
-		it 'get 200 status code' do
-			get :show, params: {id: person.id}
+		it 'Get 200 status code' do
+			get :show, params: {id: @person.id}
 			expect(response.status).to eq(200)
 		end
 	end
 
 	describe 'PATCH/PUT #update' do
-		context 'with valid attributes' do
-			it 'updates a existent person'
+		context 'With valid attributes' do
+			it 'updates a existent person' do
+				put :update, params: {id: @person.id, person: FactoryGirl.attributes_for(:person, name: "Victor Alessander")}
+				@person.reload
+				expect(assigns(:person)).to eq(@person)
+				expect(response).to redirect_to(:action => :show)
+			end
+		end
+		
+		context 'Not valid without a attribute' do
+			it 'Does not change @person attributes' do
+				put :update, params: {id: @person.id, person: FactoryGirl.attributes_for(:person,
+					name: nil,
+					birthday: '1997-04-05',
+					email: 'alessander@protonmail.com')}
+				@person.reload
+				expect(@person.name).to eq("Joaozinho")
+				expect(@person.birthday).to_not eq('1997-04-05')
+				expect(@person.email).to_not eq('alessander@protonmail.com')
+			end
+			it 'Re-renders the edit method' do
+				put :update, params: {id: @person.id, person: FactoryGirl.attributes_for(:person,
+					name: nil,
+					birthday: '1997-04-05',
+					email: 'alessander@protonmail.com')}
+				expect(response).to render_template(:edit)
+			end
 		end
 	end
 
 	describe 'DELETE #destroy' do
-		it 'destroy a existent person'
-		it 'redirects to person#index'
+		it 'Destroy a existent person' do
+			expect{
+				delete :destroy, params: {id: @person.id}
+				}.to change{Person.count}.by(-1)
+		end
+		it 'Redirects to person#index' do
+			delete :destroy, params: {id: @person.id}
+			expect(response).to redirect_to(:action => :index)
+		end
 	end
 
 end
