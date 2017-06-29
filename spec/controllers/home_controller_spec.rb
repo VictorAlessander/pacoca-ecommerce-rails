25345489a1b7957f13663eb284622393b2de806f
Victor Alessander <alessander@protonmail.com>
2017-06-28 23:29:08 -0300

    Implemented another test

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -3,11 +3,15 @@ require 'rails_helper'
 
 RSpec.describe HomeController do
 
-	describe 'GET #index' do
+	context 'GET #index' do
 		it "renders the :index view" do
 			get :index
 			expect(response).to render_template(:index)
 		end
-	end
 
+		it "Get 200 status code" do
+			get :index
+			expect(response.status).to eq(200)
+		end
+	end
 end
