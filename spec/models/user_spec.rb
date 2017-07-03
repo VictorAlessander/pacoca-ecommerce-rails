f44dd86d3d7556f9b2e8a931b3ac7900a6e7be70
alessander <alessander@protonmail.com>
2017-07-03 11:31:33 -0400

    Created association test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) {create :user}
+
+  describe "Associations" do
+    it {should have_one(:person)}
+  end
 end
