5d32aa9550a441b8ecac0b3193bd4f843ec9a98f
Victor Alessander <alessander@protonmail.com>
2017-07-06 22:29:59 -0300

    Starting to create some data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts("Generating data for Category...")
+10.times do |x|
+	categories = Category.create(
+		cod: Faker::Number.number(3),
+		name: Faker::Commerce.department(1))
+end
+puts("Generating data for Category...[OK]")
+
+puts("Generating data for Product...")
+Category.all.each do |category|
+	Random.rand(1..2).times do |x|
+		products = Product.create(
+			cod: Faker::Number.number(3),
+			name: Faker::Commerce.product_name,
+			price: Faker::Commerce.price,
+			category: category)
+	end
+end
+puts("Generating data for Product...[OK]")
