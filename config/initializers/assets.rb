ee8e457fdc952aa6cd4c616688abf9e0983d19e2
Victor Alessander <alessander@protonmail.com>
2017-07-08 15:09:13 -0300

    Moved third-party libraries to the vendor folder for best practices

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -13,8 +13,8 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
 
-Rails.application.config.assets.precompile += %w( bulma.css )
-Rails.application.config.assets.precompile += %w( hero.css )
-Rails.application.config.assets.precompile += %w( bulma.js )
-Rails.application.config.assets.precompile += %w( masks.js )
+Rails.application.config.assets.precompile += %w( bulma.css
+																									hero.css
+																									bulma.js
+																									masks.js)
 #Rails.application.config.assets.precompile << /\.(?:svg|eot|woff|ttf)\z/
