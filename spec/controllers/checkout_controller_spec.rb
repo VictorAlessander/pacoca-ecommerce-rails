736599cebc3592765394487e91df3a4f525740ad
Victor Alessander <alessander@protonmail.com>
2017-07-29 22:13:39 -0300

    Added one more test to 'index' view

--- spec/controllers/checkout_controller_spec.rb
+++ spec/controllers/checkout_controller_spec.rb
@@ -5,11 +5,16 @@ RSpec.describe CheckoutController, type: :controller do
   let(:user) {create :user}
 
   describe "GET #index" do
+    it "renders the :index view" do
+      sign_in user
+      get :index
+      expect(response).to render_template(:index)
+    end
+
     it "returns http success" do
       sign_in user
       get :index
       expect(response).to have_http_status(:success)
     end
   end
-
 end
