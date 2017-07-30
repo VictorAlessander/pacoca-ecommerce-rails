ebc3db98acb4d8c4b3bdd8ffb92cd0d8a1b1ec63
Victor Alessander <alessander@protonmail.com>
2017-07-29 22:16:09 -0300

    First commit

---
+++ spec/controllers/products_controller_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+RSpec.describe ProductsController, type: :controller do
+  let(:user) {create user}
+
+  describe "GET #index" do
+    it "renders the :index view" do
+      get :index
+      expect(response).to render_template(:index)
+    end
+
+    it "returns 200 status code" do
+      get :index
+      expect(response).to have_http_status(200)
+    end
+  end
+
+  describe "GET #add_product" do
+    context "if product already exists in cart" do
+      it "increment product quantity in cart"
+    end
+
+    context "if product does not exists in cart" do
+      it "add product to cart"
+    end
+  end
+end
